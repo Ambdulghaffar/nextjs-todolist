42d579b28a4688ce18f089384737d767ebbaa8f0
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-12-06 13:25:43 +0100

    feat: add navbar and banner components

---
+++ src/components/home/banner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <>
+      <div>
+        <p>to do list</p>
+        <p>optimizer</p>
+        <p>
+          This platform can determine the importance of your tasks., allowing
+          you to be more efficient in your daily operations.
+        </p>
+        
+      </div>
+    </>
+  );
+}
---
+++ src/components/home/home-navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+
+export default function HomeNavbar() {
+
+  const linkData = [
+    { name: "home", href: "#" , className: "text-orange-500"},
+    { name: "features", href: "#" },
+    { name: "pricing", href: "#" },
+    { name: "testimonials", href: "#" },
+  ];
+
+  return (
+    <>
+      <div className="flex justify-between items-center ">
+        <div className="flex items-center gap-10">
+          <div className="flex items-center gap-3">
+            <p>icon</p>
+            <p className="font-bold">ToDo<span className="text-orange-500">lt</span></p>
+          </div>
+          {linkData.map((link,index)=>(
+          <div key={index} className="flex items-center gap-4 font-bold capitalize">
+            <Link href={link.href} className={link.className ? link.className : ""}>{link.name}</Link>
+          </div>
+          ))}
+        </div>
+          <Button className="bg-orange-500 border rounded-full px-7 py-6 capitalize text-sm text-gray-200">dowanload app</Button>
+      </div>
+    </>
+  );
+}
