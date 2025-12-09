f15d0b3395cebbde51a8ff95ef681abff1c12c64
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-12-09 18:10:41 +0100

    Clean main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import Banner from "@/components/home/banner";
-import Footer from "@/components/home/footer";
-import NavbarHome from "@/components/home/navbar-home";
+
 
 export default function Home() {
   return (
-    <>
-      <div className="px-10 pt-4">
-        <NavbarHome />
-        <Banner />
-      </div>
-      <Footer />
-    </>
-  );
-}
+    <h1>Bonsoir</h1>
+    );
+  }
