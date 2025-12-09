646c3e35522bcbc145b683055be4f18eba9b1598
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-12-09 22:09:31 +0100

    fix: create home page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Banner from "@/components/home/banner";
+import Footer from "@/components/home/footer";
+import NavbarHome from "@/components/home/navbar-home";
 
 
 export default function Home() {
   return (
     <>
-    <h1>salut</h1>
+      <div className="px-10 pt-4">
+        <NavbarHome />
+        <Banner />
+      </div>
+      <Footer/>
     </>
   );
 }
